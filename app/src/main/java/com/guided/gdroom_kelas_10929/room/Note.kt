eb0135d9593796ee04de53d135144349bf75cd57
ChopChoppp23 <alfredo.tanjaya03@gmail.com>
2022-09-20 09:08:05 +0700

    Guided Room

---
+++ app/src/main/java/com/guided/gdroom_kelas_10929/room/Note.kt
@@ -0,0 +1,13 @@
+package com.guided.gdroom_kelas_10929.room
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+
+data class Note (
+    @PrimaryKey(autoGenerate = true )
+    val id: Int,
+    val title: String,
+    val note: String
+    )
